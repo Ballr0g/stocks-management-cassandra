e9814af6808e11645a7bfdde2aa3d11c44a8d44e
ballr0g <theonlytrueballr0g@gmail.com>
2025-01-20 02:00:50 +0300

    [STCK-001] Added extension methods for mapping from-to DTOs.

---
+++ src/main/kotlin/io/stocks/inc/management/extensions/CreateStockQuoteDtoExtensions.kt
@@ -0,0 +1,31 @@
+package io.stocks.inc.management.extensions
+
+import io.stocks.inc.management.generated.model.PostQuoteCreatedResponseDto
+import io.stocks.inc.management.generated.model.PostQuoteRequestDto
+import io.stocks.inc.management.model.PeriodIdProvider
+import io.stocks.inc.management.model.StockQuoteEntry
+import io.stocks.inc.management.model.StockQuoteRequest
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.ZoneId
+
+fun PostQuoteRequestDto.toModel(
+    periodIdProvider: PeriodIdProvider =
+        PeriodIdProvider {
+            val currentTimeUtc = LocalDate.now(ZoneId.of("UTC"))
+            "${currentTimeUtc.dayOfYear}-${currentTimeUtc.monthValue}-${currentTimeUtc.year}"
+        },
+): StockQuoteRequest =
+    StockQuoteRequest(
+        isin = this.isin,
+        periodId = periodIdProvider.currentPeriodId(),
+        bid = bid,
+        ask = ask,
+        quoteTime = LocalDateTime.now(ZoneId.of("UTC")),
+    )
+
+fun StockQuoteEntry.toDto(): PostQuoteCreatedResponseDto =
+    PostQuoteCreatedResponseDto(
+        isin = this.isin,
+        elvl = energyLevel,
+    )
