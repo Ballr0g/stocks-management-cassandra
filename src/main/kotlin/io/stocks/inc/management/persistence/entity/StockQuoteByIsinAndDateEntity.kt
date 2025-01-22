b18699a71bd1fab44100ce3fba4d2abdc4729511
ballr0g <theonlytrueballr0g@gmail.com>
2025-01-22 12:17:08 +0300

    [STCK-001] Finalized Cassandra entities metadata setup.

--- src/main/kotlin/io/stocks/inc/management/persistence/entity/StockQuoteByIsinAndDateEntity.kt
+++ src/main/kotlin/io/stocks/inc/management/persistence/entity/StockQuoteByIsinAndDateEntity.kt
@@ -2,21 +2,27 @@ package io.stocks.inc.management.persistence.entity
 
 import org.springframework.data.cassandra.core.cql.Ordering
 import org.springframework.data.cassandra.core.cql.PrimaryKeyType
+import org.springframework.data.cassandra.core.mapping.Column
 import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn
 import org.springframework.data.cassandra.core.mapping.Table
 import java.math.BigDecimal
 import java.time.LocalDateTime
 
-@Table("quotes_by_isin_and_date")
+@Table(keyspace = "stocks", value = "quotes_by_isin_and_date")
 data class StockQuoteByIsinAndDateEntity(
-    @PrimaryKeyColumn(type = PrimaryKeyType.PARTITIONED, ordinal = 0) val isin: String,
-    @PrimaryKeyColumn(type = PrimaryKeyType.PARTITIONED, ordinal = 1) val periodId: String,
-    @PrimaryKeyColumn(
+    @field:PrimaryKeyColumn(type = PrimaryKeyType.PARTITIONED, ordinal = 0) val isin: String,
+    @field:PrimaryKeyColumn(
+        type = PrimaryKeyType.PARTITIONED,
+        name = "period_id",
+        ordinal = 1,
+    ) val periodId: String,
+    @field:PrimaryKeyColumn(
         type = PrimaryKeyType.CLUSTERED,
+        name = "quote_time",
         ordinal = 2,
         ordering = Ordering.DESCENDING,
     ) val quoteTime: LocalDateTime,
-    @PrimaryKeyColumn(type = PrimaryKeyType.CLUSTERED, ordinal = 3) val bid: BigDecimal,
-    @PrimaryKeyColumn(type = PrimaryKeyType.CLUSTERED, ordinal = 4) val ask: BigDecimal,
-    val energyLevel: BigDecimal,
+    @field:PrimaryKeyColumn(type = PrimaryKeyType.CLUSTERED, ordinal = 3) val bid: BigDecimal,
+    @field:PrimaryKeyColumn(type = PrimaryKeyType.CLUSTERED, ordinal = 4) val ask: BigDecimal,
+    @field:Column("energy_level") val energyLevel: BigDecimal,
 )
--- src/main/kotlin/io/stocks/inc/management/persistence/entity/StockQuoteEnergyLevelEntity.kt
+++ src/main/kotlin/io/stocks/inc/management/persistence/entity/StockQuoteEnergyLevelEntity.kt
@@ -1,13 +1,14 @@
 package io.stocks.inc.management.persistence.entity
 
+import org.springframework.data.cassandra.core.mapping.Column
 import org.springframework.data.cassandra.core.mapping.PrimaryKey
 import org.springframework.data.cassandra.core.mapping.Table
 import java.math.BigDecimal
 import java.time.LocalDateTime
 
-@Table("energy_level_by_isin")
+@Table(keyspace = "stocks", value = "energy_level_by_isin")
 data class StockQuoteEnergyLevelEntity(
-    @PrimaryKey val isin: String,
-    val energyLevel: BigDecimal,
-    val quoteTime: LocalDateTime,
+    @field:PrimaryKey val isin: String,
+    @field:Column("energy_level") val energyLevel: BigDecimal,
+    @field:Column("quote_time") val quoteTime: LocalDateTime,
 )
